b95218070d5d5fb256849d6d6656f85c529592d5
Asier97am <asier.aranda.malagon@gmail.com>
2021-10-25 17:57:20 +0200

    añadiendo botones

---
+++ app/src/main/java/com/asier/aranda/calculadorafinal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.asier.aranda.calculadorafinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
